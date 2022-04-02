45dbce2f386b12252114ea5842dee56ebc002657
shiki <941728409@qq.com>
2022-04-02 19:18:09 +0800

    2

---
+++ src/main/java/writer/WriterTest.java
@@ -0,0 +1,32 @@
+package writer;
+
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class WriterTest {
+    @Test
+    public void write(){
+        String  srcPath="src/main/resources/hello222.txt";
+        FileWriter fileWriter =null;
+
+        try {
+            fileWriter =new FileWriter(srcPath,true);
+
+            String str ="hello shiki";
+            fileWriter.write(str);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                fileWriter.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+}
