3b785ab1d0f82f8d6231c3a23ed9ad5f996eb616
shiki <941728409@qq.com>
2022-04-05 22:20:08 +0800

    添加了能够拷贝文件的功能222

--- src/main/java/outputStream/MyBufferedCopy.java
+++ src/main/java/outputStream/MyBufferedCopy.java
@@ -11,13 +11,13 @@ public class MyBufferedCopy {
 
         //创建缓冲变量
         byte[] bytes = new byte[1024];
-
         int readLen = 0;
+
+
 
         //创建流
         BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream(srcPath));
         BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream(tarPath));
-
         while ((readLen= bufferedInputStream.read(bytes)) != -1 ){
             bufferedOutputStream.write(bytes,0,readLen);
         }
